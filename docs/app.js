e5074a09765004bc451d2870f7b09b56b18dfb32
Cole Snyder <csnyder@robinsonmfg.com>
2025-11-04 13:27:46 -0500

    Backend Implementation 1

--- docs/app.js
+++ docs/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:4000/api/auth";
+const API_URL = "https://project4-forum-backend.onrender.com/api/auth";
 
 function toggleForm(type) {
   document.getElementById("registerForm").classList.toggle("active", type === "register");
